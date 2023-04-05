6fe46295b4d70defd8077c204305945f592db68e
Perplexed13 <yedekguler@gmail.com>
2023-04-05 08:23:18 +0300

    Sign out

--- Messenger/Controllers/ConversationsVC.swift
+++ Messenger/Controllers/ConversationsVC.swift
@@ -12,9 +12,7 @@ class ConversationsVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        view.backgroundColor = .red
-                
+                        
     }
     
     override func viewDidAppear(_ animated: Bool) {
